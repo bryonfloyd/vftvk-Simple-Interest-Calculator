b2035338de106b0d1a7f91c4d7939edc4fe12306
Bryon Floyd <66990394+bryonfloyd@users.noreply.github.com>
2021-02-22 21:22:52 +0800

    Update script.js
    - created function for calculating interest
    - created function for displaying value for slider bar

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
+
 function compute()
-{
-    p = document.getElementById("principal").value;
+{  
+        var principal = document.getElementById("principal").value;
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+
+        var d = new Date();
+        var n = d.getFullYear();
     
-}
+        var m = n + parseInt(years);
+        var ir = rate/100;
+    
+        if (principal == "" || principal <= 0){
+           alert("Enter a positive number.");   
+        } else{
+           var interest = principal*ir*years;   
+           
+           document.getElementById('result').innerHTML = 
+                                'If you deposit <mark>' + principal + '</mark>,<br>' 
+                                + 'at an interest rate of <mark>' + rate + '%</mark>.<br>'
+                                + 'You will receive an amount of <mark>' + interest + '</mark>,<br>'
+                                + 'in the year <mark>' + m + '</mark>';
+          }               
         
+}
+
+function slider() 
+{
+    var rate = document.getElementById("rate").value;
+    
+    document.getElementById('slideDisplay').innerHTML = rate+'%';
+    
+}   
